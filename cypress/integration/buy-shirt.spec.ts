4049cebc0572bd877f7d7228db7907a632267a7a
Duvan Ganan <duvan.gananp@perficient.com>
2022-05-27 13:46:16 -0500

    feat: adding AAA patern
    adding AAA patern

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -3,18 +3,29 @@ import {
   LoginPage, AddressPage, ShippingPage, PaymentPage,
 } from "../page/index";
 
-const menuContentPage = new MenuContentPage();
-const productListPage = new ProductListPage();
-const shoppingCartPage = new ShoppingCartPage();
-const loginPage = new LoginPage();
-const addressPage = new AddressPage();
-const shippingPage = new ShippingPage();
-const paymentPage = new PaymentPage();
+let menuContentPage: MenuContentPage;
+let productListPage: ProductListPage;
+let shoppingCartPage: ShoppingCartPage;
+let loginPage: LoginPage;
+let addressPage: AddressPage;
+let shippingPage: ShippingPage;
+let paymentPage: PaymentPage;
 
-const email = "aperdomobo@gmail.com";
-const pass = "WorkshopProtractor";
+let email: string;
+let pass: string;
 
 describe("Buy a t-shirt", () => {
+  before(() => {
+    menuContentPage = new MenuContentPage();
+    productListPage = new ProductListPage();
+    shoppingCartPage = new ShoppingCartPage();
+    loginPage = new LoginPage();
+    addressPage = new AddressPage();
+    shippingPage = new ShippingPage();
+    paymentPage = new PaymentPage();
+    email = "aperdomobo@gmail.com";
+    pass = "WorkshopProtractor";
+  });
   it("then the t-shirt should be bought", () => {
     // Arrange
     menuContentPage.visitMenuContentPage();
