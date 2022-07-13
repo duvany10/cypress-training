61d0ef0f9b44e314ce00065000442d05fbb4ac03
Duvan Ganan <duvan.gananp@perficient.com>
2022-07-13 10:00:24 -0500

    feat: lint

--- cypress/integration/personal-form.spec.ts
+++ cypress/integration/personal-form.spec.ts
@@ -1,15 +1,14 @@
-import { PersonalFormPage } from "../page/personal-form.page";
+import {PersonalFormPage} from "../page/personal-form.page";
 let personalFormPage: PersonalFormPage;
 
 describe("Personal form page", () => {
   before(() => {
     personalFormPage = new PersonalFormPage();
-  })
+  });
   it("should have a title", () => {
-
     // Arrange
-    const reading = '2';
-    const music = '3';
+    const reading = "2";
+    const music = "3";
     const personalInformation = {
       name: "Duvan",
       lastName: "Ganan",
@@ -18,19 +17,19 @@ describe("Personal form page", () => {
       mobileNumber: "3656589156",
       hobbies: [reading, music],
       currentAddress: "Av siempreViva # 123",
-    }
+    };
     cy.visit("https://demoqa.com/automation-practice-form");
 
-    // Act 
+    // Act
     personalFormPage.fillForm(personalInformation);
 
     // Assert
-    cy.get('tbody tr')
-    .should("contain.text", `${personalInformation.name} ${personalInformation.lastName}`)
-    .should("contain.text", personalInformation.email)
-    .should("contain.text", personalInformation.gender)
-    .should("contain.text", personalInformation.mobileNumber)
-    .should("contain.text", "Reading, Music")
-    .should("contain.text", personalInformation.currentAddress);    
+    cy.get("tbody tr")
+        .should("contain.text", `${personalInformation.name} ${personalInformation.lastName}`)
+        .should("contain.text", personalInformation.email)
+        .should("contain.text", personalInformation.gender)
+        .should("contain.text", personalInformation.mobileNumber)
+        .should("contain.text", "Reading, Music")
+        .should("contain.text", personalInformation.currentAddress);
   });
 });
