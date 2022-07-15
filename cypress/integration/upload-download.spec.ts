361485438c8ed143bf65ca0858fe4178408394f8
Duvan Ganan <duvan.gananp@perficient.com>
2022-07-15 07:52:56 -0500

    feat: lint
    lint

--- cypress/integration/upload-download.spec.ts
+++ cypress/integration/upload-download.spec.ts
@@ -1,20 +1,20 @@
-import { MenuContentPage, UploadPage } from '../page';
+import {UploadPage} from "../page";
 
 let uploadPage: UploadPage;
 
-describe('Upload File', () => {
-    before(() => {
-        uploadPage = new UploadPage();
-    })
-    it("Should have a Tittle", () => {
-        //Arrange
-        const fileName = 'example.json';
+describe("Upload File", () => {
+  before(() => {
+    uploadPage = new UploadPage();
+  });
+  it("Should have a Tittle", () => {
+    // Arrange
+    const fileName = "example.json";
 
-        //Act
-        uploadPage.visitUploadDemoSite();
-        uploadPage.uploadFile(fileName);
+    // Act
+    uploadPage.visitUploadDemoSite();
+    uploadPage.uploadFile(fileName);
 
-        //Assert
-        uploadPage.getTittle().should('contain.text', fileName);
-    })
-})
+    // Assert
+    uploadPage.getTittle().should("contain.text", fileName);
+  });
+});
